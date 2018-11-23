6d30e81bc2b7e8b2b2bc6ee99b798c09f6f0c270
root <root@bastion.8156.example.opentlc.com>
2018-11-23 09:23:05 +0000

    Adjusted script paths

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Copy a new hosts file to /etc/ansible/hosts"
-cp -a /root/ocp_advanced_deployment_homework/hosts /etc/ansible/hosts
+cp -a /root/openshift3-advanced-deployment-homework/hosts /etc/ansible/hosts
 
 echo "Set the current GUID to generate the inventory"
 GUID=`hostname|awk -F. '{print $2}'`
