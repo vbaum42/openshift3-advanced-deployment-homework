168b2cfb3836ac4b6eb0a56fadd2f49a348444ba
Vladimir Baumgartner <vladimir.baumgartner@devoteam.com>
2018-11-27 11:28:03 +0100

    Removed references to separate cicd project

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -3,5 +3,3 @@
 ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/adhoc/uninstall.yml
 ansible nodes -a "rm -rf /etc/origin"
 ansible nfs -a "rm -rf /srv/nfs/*"
-
-rm -rf /root/openshift3-cd-demo
